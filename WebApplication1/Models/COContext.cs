f7ef6a27ff02bb00fd97fddd63c2d3f27a223b68
megatheron <dupreeztheron@gmail.com>
2022-09-07 18:36:49 +0200

    progress

---
+++ WebApplication1/Models/COContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class COContext : DbContext
+    {
+        public COContext()
+        {
+        }
+
+        public COContext(DbContextOptions<COContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Device> Device { get; set; }
+        public virtual DbSet<Zone> Zone { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=projecttwee.database.windows.net;Initial Catalog=CO;User ID=dupreeztheron;Password=ekismoegenwilslaap123!;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId)
+                    .HasColumnName("CategoryID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryName).IsRequired();
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(e => e.DeviceId)
+                    .HasColumnName("DeviceID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ZoneId).HasColumnName("ZoneID");
+            });
+
+            modelBuilder.Entity<Zone>(entity =>
+            {
+                entity.Property(e => e.ZoneId)
+                    .HasColumnName("ZoneID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ZoneName).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
